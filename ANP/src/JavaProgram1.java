183f0f1a41e8877691b04da0b30fd558f56e4ab1
Dell <Dell@DESKTOP-8TTO4IP>
2022-10-27 09:40:10 +0530

    Version1 updated

---
+++ ANP/src/JavaProgram1.java
@@ -0,0 +1,11 @@
+
+public class JavaProgram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Morning");
+		System.out.println("Hello World");
+
+	}
+
+}
